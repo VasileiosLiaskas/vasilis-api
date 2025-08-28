7eb440bd45089ca39ce7b7bd1145fa9cf0380850
VasileiosLiaskas <vasileiosliaskas@gmail.com>
2025-08-28 20:39:00 +0300

    logs

--- src/main/java/vasilis/vasilis/VasilisApplication.java
+++ src/main/java/vasilis/vasilis/VasilisApplication.java
@@ -1,13 +1,24 @@
 package vasilis.vasilis;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VasilisApplication {
+public class VasilisApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(VasilisApplication.class, args);
+    }
+    @Override
+    public void run(String... args) {
+        System.out.println("---- ENV VAR DEBUG ----");
+        System.out.println("MYSQLHOST=" + System.getenv("MYSQLHOST"));
+        System.out.println("MYSQLUSER=" + System.getenv("MYSQLUSER"));
+        System.out.println("MYSQLDATABASE=" + System.getenv("MYSQLDATABASE"));
+        System.out.println("MYSQLPORT=" + System.getenv("MYSQLPORT"));
+        System.out.println("SPRING_PROFILES_ACTIVE=" + System.getenv("SPRING_PROFILES_ACTIVE"));
+        System.out.println("------------------------");
     }
 
 }
